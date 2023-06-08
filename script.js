ad531027be30fb25d89a4b6fa0761497a820b9c3
nikhilNathwani <nnathwani36@gmail.com>
2023-06-08 10:07:26 -0700

    Fixed bug where some chunks where getting the wrong number of words, it was because some spaces were actually newlines or tabs and this getting ignored by the split(' ') function, so I replaced the space with a regex for all whitespace i.e. split(/\s+/). Also added styling to alternate blue/red coloring across consecutive chunks

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-function getNumberOfChunks(numWords, wordsPerChunk) {
-	return Math.ceil(numWords / wordsPerChunk);
-}
+document
+	.getElementById("chunk-size-form")
+	.addEventListener("submit", function (event) {
+		event.preventDefault(); // Prevent form submission
 
-function wrapWordsInDivs(wordsPerChunk) {
-	var loremDiv = document.getElementById("lorem");
-	var wordDivs = document.querySelectorAll("div.word");
-	var totalWords = wordDivs.length;
-	var currentWordDivIndex = 0;
-	var chunkCount = getNumberOfChunks(totalWords, wordsPerChunk);
-	var chunkDivs = [];
+		const chunkSize = parseInt(document.getElementById("chunk-size").value);
+		const text = document.getElementById("text").textContent;
+		const words = text.trim().split(/\s+/);
 
-	for (var i = 0; i < chunkCount; i++) {
-		var chunk = document.createElement("span");
-		chunk.classList.add("chunk");
-		for (var wordIndex = 0; wordIndex < wordsPerChunk; wordIndex++) {
-			if (currentWordDivIndex < totalWords) {
-				chunk.appendChild(wordDivs[currentWordDivIndex]);
-				currentWordDivIndex++;
-			} else {
-				chunk.classList.add("shortened-chunk");
-			}
+		let formattedText = "";
+		for (let i = 0; i < words.length; i += chunkSize) {
+			const chunk = words.slice(i, i + chunkSize);
+			console.log(i, i + chunkSize, chunk);
+			const colorClass = (i / chunkSize) % 2 === 0 ? "blue" : "red";
+			const borderStyle = colorClass === "blue" ? "solid" : "dashed";
+
+			formattedText +=
+				'<span class="' +
+				colorClass +
+				'" style="border: 1px ' +
+				borderStyle +
+				';">' +
+				chunk.join(" ") +
+				"</span> ";
 		}
-		loremDiv.append(chunk);
-	}
-}
 
-// Call the function with wordsPerChunk = 3
-wrapWordsInDivs(3);
+		document.getElementById("text").innerHTML = formattedText.trim();
+	});
+0;
